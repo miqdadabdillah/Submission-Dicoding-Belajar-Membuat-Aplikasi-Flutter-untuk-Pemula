b6492af00ddc92d836697ac1094cf49be0c29d6c
miqdad279 <miqdadabdillah279@gmail.com>
2023-12-02 12:50:55 +0700

    modified:   lib/pages/started_page.dart

--- lib/pages/started_page.dart
+++ lib/pages/started_page.dart
@@ -15,78 +15,89 @@ class StartedPage extends StatelessWidget {
                   image: AssetImage('assets/bg.png'), fit: BoxFit.cover),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 30, top: 50, right: 30),
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                    top: 40, bottom: 40, left: 30, right: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Cari dan Bagikan Rekomendasi Buku Pilihanmu!',
-                      style: GoogleFonts.poppins(
-                          fontSize: constraints.maxWidth > 600 ? 32 : 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.normal),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Cari dan Bagikan Rekomendasi Buku Pilihanmu!',
+                          style: GoogleFonts.poppins(
+                              fontSize: constraints.maxWidth > 600 ? 32 : 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          'Dapatkan inspirasi sekarang!',
+                          style: GoogleFonts.poppins(
+                              fontSize: constraints.maxWidth > 600 ? 14 : 10,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Dapatkan inspirasi sekarang!',
-                      style: GoogleFonts.poppins(
-                          fontSize: constraints.maxWidth > 600 ? 14 : 10,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300),
-                    ),
-                    SizedBox(height: constraints.maxWidth > 600 ? 400 : 400),
-                    Center(
-                      child: SizedBox(
-                        width: constraints.maxWidth > 600
-                            ? 400
-                            : constraints.maxWidth * 0.8,
-                        height: 50,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(66))),
-                          onPressed: () {},
-                          child: Text(
-                            'Get Started',
-                            style: GoogleFonts.poppins(
-                                color: const Color(0xff404040),
-                                fontSize: constraints.maxWidth > 600 ? 14 : 10,
-                                fontWeight: FontWeight.w500),
+                    Column(
+                      children: [
+                        Center(
+                          child: SizedBox(
+                            width: constraints.maxWidth > 600
+                                ? 400
+                                : constraints.maxWidth * 0.8,
+                            height: 50,
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(66))),
+                              onPressed: () {},
+                              child: Text(
+                                'Get Started',
+                                style: GoogleFonts.poppins(
+                                    color: const Color(0xff404040),
+                                    fontSize:
+                                        constraints.maxWidth > 600 ? 14 : 10,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        SizedBox(height: constraints.maxWidth > 600 ? 16 : 16),
+                        Center(
+                          child: SizedBox(
+                            width: constraints.maxWidth > 600
+                                ? 400
+                                : constraints.maxWidth * 0.8,
+                            height: 50,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                  side: const BorderSide(color: Colors.white),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(66))),
+                              onPressed: () {},
+                              child: Text(
+                                'Sign In',
+                                style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize:
+                                        constraints.maxWidth > 600 ? 14 : 10,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                    SizedBox(height: constraints.maxWidth > 600 ? 16 : 16),
-                    Center(
-                      child: SizedBox(
-                        width: constraints.maxWidth > 600
-                            ? 400
-                            : constraints.maxWidth * 0.8,
-                        height: 50,
-                        child: OutlinedButton(
-                            style: OutlinedButton.styleFrom(
-                                side: const BorderSide(color: Colors.white),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(66))),
-                            onPressed: () {},
-                            child: Text(
-                              'Sign In',
-                              style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontSize:
-                                      constraints.maxWidth > 600 ? 14 : 10,
-                                  fontWeight: FontWeight.w500),
-                            )),
-                      ),
-                    )
                   ],
-                );
-              },
-            ),
+                ),
+              );
+            },
           )
         ],
       ),
